2cd7b4f03c94bba84d8b3fcf1888891c47caead4
ScienceMind11 <sciencemind@icloud.com>
2025-04-13 18:59:46 -0400

    Add deep zombie base

---
+++ src/main/java/dev/jade/backbone/entity/DeepZombieEntity.java
@@ -0,0 +1,15 @@
+package dev.jade.backbone.entity;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.world.World;
+
+public class DeepZombieEntity extends ZombieEntity {
+
+    public DeepZombieEntity(EntityType<? extends ZombieEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+
+
+}
