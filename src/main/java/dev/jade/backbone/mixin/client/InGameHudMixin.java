2c26414df7ae7188fa6bd4e9854b75e1f93b8212
ScienceMind11 <sciencemind@icloud.com>
2025-07-04 11:03:14 -0400

    remove xyz crossshair
    Took 1 hour 32 minutes

---
+++ src/main/java/dev/jade/backbone/mixin/client/InGameHudMixin.java
@@ -0,0 +1,19 @@
+package dev.jade.backbone.mixin.client;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.gui.hud.DebugHud;
+import net.minecraft.client.gui.hud.InGameHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(InGameHud.class)
+public class InGameHudMixin {
+
+    @WrapOperation(method = "renderCrosshair", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/DebugHud;shouldShowDebugHud()Z"))
+    private boolean backbone$cancelDebugCrosshair(DebugHud instance, Operation<Boolean> original) {
+        return false;
+    }
+
+}
